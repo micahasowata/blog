eb6988fa7cea1988d336052dde6d9743dcf7a3fe
Micah Asowata <andrewgary963@gmail.com>
2024-03-10 17:59:16 +0000

    feat: make max request size configurable

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,12 +2,14 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	Address string
+	MaxSize int
 }
 
 func New() (*Config, error) {
@@ -16,8 +18,14 @@ func New() (*Config, error) {
 		return nil, err
 	}
 
+	size, err := strconv.Atoi(os.Getenv("MAX_SIZE"))
+	if err != nil {
+		return nil, err
+	}
+
 	cfg := &Config{
 		Address: os.Getenv("ADDR"),
+		MaxSize: size,
 	}
 	return cfg, nil
 }
