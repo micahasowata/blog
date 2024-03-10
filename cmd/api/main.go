836de9b42caabd75a4bfeab43d6fc0dcbad1854b
Micah Asowata <andrewgary963@gmail.com>
2024-03-10 18:00:04 +0000

    feat: add json parser to the app dependencies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 
 	"github.com/micahasowata/blog/internal/config"
+	"github.com/micahasowata/jason"
 	"go.uber.org/zap"
 )
 
 type application struct {
+	*jason.Jason
 	logger *zap.Logger
 	config *config.Config
 }
@@ -24,6 +26,7 @@ func main() {
 	}
 
 	app := &application{
+		Jason:  jason.New(int64(config.MaxSize), false, true),
 		logger: logger,
 		config: config,
 	}
