8c396f0290c9fa2e624a96e704bb7945cd84216b
Micah Asowata <andrewgary963@gmail.com>
2024-03-11 14:23:06 +0000

    feat: add and test validation checks to handler

--- cmd/api/user_handlers.go
+++ cmd/api/user_handlers.go
@@ -1,6 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/micahasowata/jason"
+)
 
 func (app *application) registerUser(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name" validate:"required,lte=150"`
+		Username string `json:"username" validate:"required,gte=2,lte=25"`
+		Email    string `json:"email" validate:"required,email,lte=150"`
+	}
+
+	err := app.Read(w, r, &input)
+	if err != nil {
+		app.badRequestHandler(w, err)
+		return
+	}
+
+	err = app.validate.Struct(&input)
+	if err != nil {
+		app.validationErrHandler(w, err)
+	}
+
+	err = app.Write(w, http.StatusOK, jason.Envelope{"user": input}, nil)
+	if err != nil {
+		app.writeErrHandler(w, err)
+		return
+	}
 }
--- cmd/api/user_handlers_test.go
+++ cmd/api/user_handlers_test.go
@@ -6,6 +6,10 @@ import (
 	"testing"
 
 	"github.com/gavv/httpexpect/v2"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/micahasowata/blog/internal/config"
 	"github.com/micahasowata/blog/internal/db"
@@ -31,11 +35,20 @@ func setupApp(t *testing.T, db *pgxpool.Pool) *application {
 		MaxSize: 1_048_576,
 	}
 
+	localeEN := en.New()
+	universal := ut.New(localeEN, localeEN)
+	translator, ok := universal.GetTranslator("en")
+	require.NotEmpty(t, ok)
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	en_translations.RegisterDefaultTranslations(validate, translator)
+
 	app := &application{
-		Jason:  jason.New(int64(cfg.MaxSize), false, true),
-		logger: zap.NewExample(),
-		config: cfg,
-		models: models.New(db),
+		Jason:      jason.New(int64(cfg.MaxSize), false, true),
+		logger:     zap.NewExample(),
+		config:     cfg,
+		validate:   validate,
+		translator: translator,
+		models:     models.New(db),
 	}
 
 	return app
@@ -57,12 +70,12 @@ func TestRegisterUser(t *testing.T) {
 	}{
 		{
 			name: "valid",
-			body: `{name:"addam", username:"iamadam", email:"theaddam"}`,
+			body: `{"name": "addam","username": "iamadam","email": "theaddam@gmail.com"}`,
 			code: http.StatusOK,
 		},
 		{
 			name: "bad body",
-			body: `{password:"9L]dPa!iw8B"}`,
+			body: `{"password":"9LdPaiw8B"}`,
 			code: http.StatusBadRequest,
 		},
 	}
