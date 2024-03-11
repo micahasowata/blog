8cd8a6a4541e29cc04b1db47843f6a54f0c980de
Micah Asowata <andrewgary963@gmail.com>
2024-03-11 00:04:35 +0000

    feat: add set up and tear down db functions

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/go-chi/chi/v5 v5.0.12
+	github.com/jackc/pgx/v5 v5.5.5
 	github.com/joho/godotenv v1.5.1
 	github.com/micahasowata/jason v1.0.1
 	github.com/pseidemann/finish v1.2.0
@@ -13,10 +14,17 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/db/db.go
@@ -0,0 +1,71 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/micahasowata/blog/internal/config"
+)
+
+func NewProduction(cfg *config.Config) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	db, err := pgxpool.New(ctx, cfg.ProdDSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func NewTest(cfg *config.Config) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	db, err := pgxpool.New(ctx, cfg.TestDSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func Clean(db *pgxpool.Pool) error {
+	query := `
+	DO $$ DECLARE r RECORD;
+		BEGIN FOR r IN (
+			SELECT 
+				tablename 
+			FROM 
+				pg_tables 
+			WHERE 
+				schema_name = 'public'
+			) LOOP EXECUTE 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+		END LOOP;
+	END $$;
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	tx, err := db.BeginTx(ctx, pgx.TxOptions{
+		IsoLevel:       pgx.Serializable,
+		AccessMode:     pgx.ReadWrite,
+		DeferrableMode: pgx.NotDeferrable,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback(ctx)
+
+	_, err = tx.Exec(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
