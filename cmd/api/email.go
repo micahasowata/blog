6548b21e304266acb82aa23407ffb29fa0b3d3d8
Micah Asowata <andrewgary963@gmail.com>
2024-03-11 23:17:14 +0000

    feat: add methods to set email tasks

---
+++ cmd/api/email.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/micahasowata/blog/internal/templates"
+	"github.com/wneessen/go-mail"
+)
+
+const (
+	typeWelcomeEmail = "email:welcome"
+)
+
+type welcomeEmailPayload struct {
+	Name string
+	To   string
+	From string
+}
+
+func (app *application) newWelcomeEmailTask(name, to string) (*asynq.Task, error) {
+	payload, err := jsoniter.Marshal(welcomeEmailPayload{Name: name, To: to, From: app.config.From})
+	if err != nil {
+		return nil, err
+	}
+
+	return asynq.NewTask(typeWelcomeEmail, payload, asynq.MaxRetry(3)), nil
+}
+
+func (app *application) handleWelcomeEmailDelivery(ctx context.Context, t *asynq.Task) error {
+	payload := welcomeEmailPayload{}
+
+	err := jsoniter.Unmarshal(t.Payload(), &payload)
+	if err != nil {
+		return err
+	}
+
+	message := mail.NewMsg()
+
+	err = message.From(payload.From)
+	if err != nil {
+		return err
+	}
+
+	err = message.To(payload.To)
+	if err != nil {
+		return err
+	}
+
+	message.Subject("👋🏼 Welcome to Blog 👋🏼")
+
+	err = message.SetBodyHTMLTemplate(templates.Parse("welcome_email"), &payload)
+	if err != nil {
+		return err
+	}
+
+	client, err := mail.NewClient(app.config.SMTPHost, mail.WithPort(app.config.SMTPPort),
+		mail.WithSMTPAuth(mail.SMTPAuthPlain), mail.WithUsername(app.config.SMTPUsername),
+		mail.WithPassword(app.config.SMTPPassword))
+
+	if err != nil {
+		return err
+	}
+
+	err = client.DialAndSendWithContext(ctx, message)
+	if err != nil {
+		return err
+	}
+
+	return client.Close()
+}
---
+++ cmd/api/email_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewWelcomeEmailTask(t *testing.T) {
+	app := setupApp(t, nil)
+
+	task, err := app.newWelcomeEmailTask("iamadam", "adam45@gmail.com")
+	require.Nil(t, err)
+	require.NotNil(t, task)
+}
+
+func TestHandleWelcomeEmailDelivery(t *testing.T) {
+	app := setupApp(t, nil)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	task, err := app.newWelcomeEmailTask("iamadam", "adam45@gmail.com")
+	require.Nil(t, err)
+
+	err = app.handleWelcomeEmailDelivery(ctx, task)
+	require.Nil(t, err)
+
+}
