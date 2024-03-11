e725ef0e56fda5d72902d7aca418f6a4451f5337
Micah Asowata <andrewgary963@gmail.com>
2024-03-11 23:16:16 +0000

    feat: add smtp settings to app config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,9 +8,15 @@ import (
 )
 
 type Config struct {
-	Address string
-	MaxSize int
-	ProdDSN string
+	Address      string
+	MaxSize      int
+	ProdDSN      string
+	AsynqRDC     string
+	From         string
+	SMTPHost     string
+	SMTPPort     int
+	SMTPUsername string
+	SMTPPassword string
 }
 
 func New() (*Config, error) {
@@ -24,10 +30,21 @@ func New() (*Config, error) {
 		return nil, err
 	}
 
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		return nil, err
+	}
+
 	cfg := &Config{
-		Address: os.Getenv("ADDR"),
-		MaxSize: size,
-		ProdDSN: os.Getenv("PROD_DSN"),
+		Address:      os.Getenv("ADDR"),
+		MaxSize:      size,
+		ProdDSN:      os.Getenv("PROD_DSN"),
+		AsynqRDC:     os.Getenv("ASYNQ_RDC"),
+		From:         os.Getenv("FROM"),
+		SMTPHost:     os.Getenv("SMTP_HOST"),
+		SMTPPort:     port,
+		SMTPUsername: os.Getenv("SMTP_USERNAME"),
+		SMTPPassword: os.Getenv("SMTP_PASSWORD"),
 	}
 	return cfg, nil
 }
