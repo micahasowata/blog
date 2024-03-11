b348116a9fa565693b1a0bfd648b9f8f47fd9a9a
Micah Asowata <andrewgary963@gmail.com>
2024-03-11 01:39:16 +0000

    feat: add the models to the app dependencies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,18 @@ import (
 	"log"
 
 	"github.com/micahasowata/blog/internal/config"
+	"github.com/micahasowata/blog/internal/db"
+	"github.com/micahasowata/blog/internal/models"
 	"github.com/micahasowata/jason"
 	"go.uber.org/zap"
 )
 
 type application struct {
 	*jason.Jason
+
 	logger *zap.Logger
 	config *config.Config
+	models *models.Models
 }
 
 func main() {
@@ -25,10 +29,16 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	db, err := db.NewProduction(config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	app := &application{
 		Jason:  jason.New(int64(config.MaxSize), false, true),
 		logger: logger,
 		config: config,
+		models: models.New(db),
 	}
 
 	app.serve()
