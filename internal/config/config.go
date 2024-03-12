5f7bc57695901ef553a31bd1cf82a4c2be793f71
Micah Asowata <andrewgary963@gmail.com>
2024-03-12 17:33:30 +0000

    feat: add test db dsn to app config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,14 +3,13 @@ package config
 import (
 	"os"
 	"strconv"
-
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	Address      string
 	MaxSize      int
 	ProdDSN      string
+	TestDSN      string
 	AsynqRDC     string
 	From         string
 	SMTPHost     string
@@ -20,11 +19,6 @@ type Config struct {
 }
 
 func New() (*Config, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return nil, err
-	}
-
 	size, err := strconv.Atoi(os.Getenv("MAX_SIZE"))
 	if err != nil {
 		return nil, err
@@ -39,6 +33,7 @@ func New() (*Config, error) {
 		Address:      os.Getenv("ADDR"),
 		MaxSize:      size,
 		ProdDSN:      os.Getenv("PROD_DSN"),
+		TestDSN:      os.Getenv("TEST_DSN"),
 		AsynqRDC:     os.Getenv("ASYNQ_RDC"),
 		From:         os.Getenv("FROM"),
 		SMTPHost:     os.Getenv("SMTP_HOST"),
