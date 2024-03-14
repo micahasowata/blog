236bffa72761fc32f1faff771664f62f8852c39b
Micah Asowata <andrewgary963@gmail.com>
2024-03-14 18:52:05 +0000

    feat: add middleware to check for refresh tokens

--- cmd/api/middlewares_test.go
+++ cmd/api/middlewares_test.go
@@ -78,3 +78,70 @@ func TestRequireAccessToken(t *testing.T) {
 		})
 	}
 }
+
+func TestRequireRefreshToken(t *testing.T) {
+	tdb := setupDB(t)
+	defer db.Clean(tdb)
+
+	app := setupApp(t, tdb)
+
+	user := &models.Users{
+		ID:       xid.New().String(),
+		Name:     "Adam",
+		Username: "iamadam",
+		Email:    "adam45@gmail.com",
+	}
+
+	createdUser, err := app.models.Users.Insert(user)
+	require.Nil(t, err)
+
+	accessToken, err := app.newAccessToken(&tokenClaims{ID: createdUser.ID, StdClaims: nil})
+	require.Nil(t, err)
+
+	refreshToken, err := app.newRefreshToken(&tokenClaims{ID: createdUser.ID, StdClaims: nil})
+	require.Nil(t, err)
+
+	tests := []struct {
+		name   string
+		header string
+		code   int
+	}{
+		{
+			name:   "valid",
+			header: "Bearer " + refreshToken,
+			code:   http.StatusOK,
+		},
+		{
+			name:   "no token",
+			header: "Bearer ",
+			code:   http.StatusForbidden,
+		},
+		{
+			name:   "access token",
+			header: "Bearer " + accessToken,
+			code:   http.StatusForbidden,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+
+			r, err := http.NewRequest(http.MethodPut, "/", nil)
+			require.Nil(t, err)
+
+			r.Header.Set("Authorization", tt.header)
+
+			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, err := w.Write([]byte("OK"))
+				require.Nil(t, err)
+			})
+
+			app.requireRefreshToken(next).ServeHTTP(rr, r)
+
+			rs := rr.Result()
+
+			assert.Equal(t, tt.code, rs.StatusCode)
+		})
+	}
+}
