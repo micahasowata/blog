045ad4f8f72a38b9b9c134ddc376979a4caec95d
Micah Asowata <andrewgary963@gmail.com>
2024-03-15 19:45:13 +0000

    feat: add new fields to the user table

--- migrations/000002_add_users_table.up.sql
+++ migrations/000002_add_users_table.up.sql
@@ -4,6 +4,9 @@ CREATE TABLE IF NOT EXISTS users (
     updated timestamptz NOT NULL DEFAULT now(),
     name citext NOT NULL,
     username citext UNIQUE NOT NULL,
+    avatar citext NOT NULL,
+    bio citext NOT NULL,
+    links citext[] NOT NULL,
     email citext UNIQUE NOT NULL,
     verified boolean NOT NULL DEFAULT false
 );
